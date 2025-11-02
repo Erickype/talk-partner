f465fc442488b8a4e173abc717bac02b3bb80221
Erick Carrasco <erickype@hotmail.com>
2025-11-02 12:51:07 -0500

    Feature: add sketch to request a talk

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { postTalk, type TalkResponse } from './api/talk'
+
+async function sendUserMessage() {
+  if (message.value.trim() === '') {
+    error.value = 'Provide a value';
+    return;
+  }
+
+  const response: TalkResponse = await postTalk(message.value);
+  console.log(response);
+  const audio = new Audio(`http://localhost:8000/output_audio/${response.audio_file}`);
+  audio.play();
+
+  error.value = '';
+  message.value = '';
+}
+
+const message = ref('')
+const error = ref('')
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>AI Talk Partner</h1>
+  <form @submit.prevent="sendUserMessage">
+    <input type="text" v-model="message" name="message" :aria-invalid="!!error || undefined" @input="error = ''">
+    <small v-if="error" class="invalid-helper">
+      {{ error }}
+    </small>
+    <button type="submit">Send</button>
+  </form>
 </template>
 
 <style scoped></style>
